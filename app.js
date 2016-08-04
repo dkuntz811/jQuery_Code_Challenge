cac6f32dcd7c46b18f05ab5585f74377f7cf32fa
Dave Kuntz <dkuntz811@gmail.com>
2016-08-04 18:13:34 -0500

    progress

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
 $(document).ready(function(){
 
-	$('#newspy').on('click', function(event) { //on click-add a yellow <div>
-		$(this).addClass('<div>""</div>' + spy).children().last();
-		$(this).css('background-color', 'yellow');
+	$('#newspy').on('click', function() { //on click-add a yellow <div>
+		var spy = $('<div> spy ID Number</div>');
+		$('.container').append(spy);
+		 console.log("???");
 
-		console.log("???");
+		$('.container').append('<button>Change</button>');
 
-		//function () {        //I was going to build a function here to create the yellow spy info. 
+		$('Change').on('click'), function () {
+			('.container').css({background: red})
+		}
+
+});
+});
+
+
+
+
+		//function () {        //I was going to build a function here to create the yellow spy info.
 
 
 		//}
-	});
-
-
-
-
-
-
-
-
-
-});
-var spyname;
-var idNumber;
-
-var array = [spyname, idNumber]
-
-var spy = idNumber;
-
-function spyData () {
-	var spyname = spyname[0];
-	var idNumber = idNumber[0];
-	console.log(spyData);
-
-}
